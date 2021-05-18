d81a85d889fb14f92aa38664c43ba092f487522c
cinnamon <justpromptiguess@gmail.com>
2021-05-18 23:37:19 +0100

    removing button (but its still there if needed
    not sure what to put honestly

--- public/index.js
+++ public/index.js
@@ -1,28 +1,40 @@
 "use strict";
 
+let gist = "https://gist.githubusercontent.com/cinnamondev/61aa08f25fe20a4943b8c613076946bb/raw/dd56aa052d9f1a7fd41386e08f306252b579867a/data.json"
+// let gist = "./data.json"
+
 let rt;
-fetch("./data.json") // temporary , move to the gist later. (this will help us work with json resume!)
+fetch(gist)
 .then(response => response.json().then(data => {
     rt = JSON.stringify(data, null, '\t');
+    displayResume(rt);
 }))
 .catch(error => {
 // Handle error // do this later.
 console.log(error);
 });
 
-let displayResume = () => {
+let exportResume = () => {
+    // the function that exports the json resume to a pdf via the json schema project.
+
+}
+
+let displayResume = (content) => {
     // We do not need to worry bout hiding - this will be a one time thing.
-    let button = document.getElementById("resumeBtn");
+    let btn = document.getElementById("resumeBtn");
+    if (btn != null) {
+        button.style="display:none;";
+    }
+
     let resume = document.getElementById("resume");
-    resumeBtn.style = "display: none;";
     resume.innerHTML = `           
     <!--jsonresume schema is neat-->
     <pre id="code" class="fade-in"><h2 style="margin-left: 32px; margin: 5px 0 0 10 px;">Resume</h2>
         <code data-language="javascript" style="margin: 0 0 0 0;">`
-         + rt + 
+         + content + 
          `</code>
 
-    <a href="#" class="button">Download as PDF</a></pre>`
+    <a href="#" class="button" onclick="exportResume()">Download as PDF</a></pre>`
     
     Rainbow.color(); // trigger rainbow to look for things to highlight
 
