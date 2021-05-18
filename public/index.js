5b369b8b73deaefcd9e1d30f2efc0d45fe98d008
cinnamon <justpromptiguess@gmail.com>
2021-05-18 23:39:43 +0100

    enable theming button

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+window.onload = () => {color.addControls();};
+
 let gist = "https://gist.githubusercontent.com/cinnamondev/61aa08f25fe20a4943b8c613076946bb/raw/dd56aa052d9f1a7fd41386e08f306252b579867a/data.json"
 // let gist = "./data.json"
 
