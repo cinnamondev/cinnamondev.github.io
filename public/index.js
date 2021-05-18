193df1b9ff6d00d02ba9218f1d44ace417f32002
cinnamon <justpromptiguess@gmail.com>
2021-05-18 21:16:00 +0100

    resume loader functionality works - work on this bit later.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,14 @@
 "use strict";
 
-let resumetext = ""; // dynamically updated resume from gist 
+let rt;
+fetch("./data.json") // temporary , move to the gist later. (this will help us work with json resume!)
+.then(response => response.json().then(data => {
+    rt = JSON.stringify(data, null, '\t');
+}))
+.catch(error => {
+// Handle error // do this later.
+console.log(error);
+});
 
 let displayResume = () => {
     // We do not need to worry bout hiding - this will be a one time thing.
@@ -10,8 +18,10 @@ let displayResume = () => {
     resume.innerHTML = `           
     <!--jsonresume schema is neat-->
     <pre id="code" class="fade-in"><h2 style="margin-left: 32px; margin: 5px 0 0 10 px;">Resume</h2>
-        <code data-language="javascript" style="margin: 0 0 0 0;">` + resume + `
-        </code>
+        <code data-language="javascript" style="margin: 0 0 0 0;">`
+         + rt + 
+         `</code>
+
     <a href="#" class="button">Download as PDF</a></pre>`
     
     Rainbow.color(); // trigger rainbow to look for things to highlight
@@ -19,13 +29,3 @@ let displayResume = () => {
 
 
 }
-
-(function() {
-    fetch("data.json") // temporary , move to the gist later. (this will help us work with json resume!)
-
-    .then(data => {
-        resumetext = data.json();
-    }).catch(error => {
-    // Handle error // do this later.
-    });
-})
