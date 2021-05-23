a06cfda762c87a176137a7ae2c04e6c339d472d0
cinnamon <justpromptiguess@gmail.com>
2021-05-23 23:27:44 +0100

    h

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let gist = "https://gist.githubusercontent.com/cinnamondev/a5a99818256d192c2d6ca41b5f7aec3b/raw/8705a4a422a998597c9f2ce448f999ee16b6d752/resume.json";
+let gist = "https://gist.githubusercontent.com/cinnamondev/a5a99818256d192c2d6ca41b5f7aec3b/raw/9ac2717478ff2f4d5f6e529d989e107e14bf88de/resume.json";
 
 window.onload = () => {
     theme.addControls();
