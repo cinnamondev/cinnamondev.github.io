f0364e95d52a3bc7c1699e30c5d9c62fbbba81e4
cinnamon <justpromptiguess@gmail.com>
2021-05-23 21:28:30 +0100

    ugh

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let gist = "https://gist.github.com/cinnamondev/a5a99818256d192c2d6ca41b5f7aec3b/raw/80e2740ee6494f159f567ebb4793d1e6ea749d22/resume.json";
+let gist = "https://gist.githubusercontent.com/cinnamondev/a5a99818256d192c2d6ca41b5f7aec3b/raw/80e2740ee6494f159f567ebb4793d1e6ea749d22/resume.json";
 
 
 window.onload = () => {
