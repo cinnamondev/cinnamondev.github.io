39b7400da028ec912741d1f15fe11719c083293d
cinnamon <50639497+cinnamondev@users.noreply.github.com>
2021-11-14 02:27:30 +0000

    Added contacts, fixes and did optimisation
    * a
    
    * added some transisitions and stuff
    
    pls
    
    * removed old
    
    * fixes
    
    Added contact page info, fixed scroll jumping, reorg projects.
    
    * optimisation

--- old/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let gist = "https://gist.githubusercontent.com/cinnamondev/a5a99818256d192c2d6ca41b5f7aec3b/raw/072114582402df9f16123c9456357df2dd2c98e6/resume.json";
-let user = "cinnamondev";
-let rth = "kendall";
-
-window.onload = () => {
-    theme.addControls();
-}
